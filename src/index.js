fc60da02ad05f8ced589de1c9ee86a77636cb23c
Florian Serraille <florianserraille@gmail.com>
2020-11-21 23:01:09 -0300

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(Data); // output `XML Example`
+console.log(Notes); // output `CSV Example`
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
+
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Webpack', 'the', 'ring'], ' ');
+    element.classList.add('hello');
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
